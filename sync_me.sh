0f24f9661e972efb071a702f5d56ad77322094c1
MDreamer <maayan.dreamer@gmail.com>
2016-05-18 21:49:10 +1000

    set 3 auto sync files py, html and the config (etc), as of not 7push.lst is not used

--- sync_me.sh
+++ sync_me.sh
@@ -2,6 +2,12 @@
 echo starting
 while true; do
   echo rsyncing
-  rsync -avzP --files-from=7push.lst . pi@192.168.1.14:~/SpaceCorn/
-  inotifywait -r -e modify --fromfile 7push.lst
+  rsync -avzP web_i2c.py pi@192.168.1.14:~/FireBender/WebIOPi-0.7.1/frbndr/python/
+  inotifywait -r -e modify web_i2c.py
+  
+  rsync -avzP config pi@192.168.1.14:/etc/webiopi/
+  inotifywait -r -e modify config
+  
+  rsync -avzP index.html pi@192.168.1.14:~/FireBender/WebIOPi-0.7.1/frbndr/html/
+  inotifywait -r -e modify index.html
 done
