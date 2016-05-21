0cf5c2c53b153797e3fed624b4f3c1c2ba515739
MDreamer <maayan.dreamer@gmail.com>
2016-05-21 23:57:16 +1000

    got a working toggle macro with I2C to start a spark. yay

--- sync_me.sh
+++ sync_me.sh
@@ -2,12 +2,13 @@
 echo starting
 while true; do
   echo rsyncing
-  rsync -avzP web_i2c.py pi@192.168.1.14:~/FireBender/WebIOPi-0.7.1/frbndr/python/
-  inotifywait -r -e modify web_i2c.py
+  rsync -avzP web_i2c.py pi@192.168.1.16:~/FireBender/WebIOPi-0.7.1/frbndr/python/
+  #inotifywait -r -e modify web_i2c.py
   
-  rsync -avzP config pi@192.168.1.14:/etc/webiopi/
-  inotifywait -r -e modify config
+  rsync -avzP config pi@192.168.1.16:/etc/webiopi/
+  #inotifywait -r -e modify config
   
-  rsync -avzP index.html pi@192.168.1.14:~/FireBender/WebIOPi-0.7.1/frbndr/html/
-  inotifywait -r -e modify index.html
+  rsync -avzP index.html pi@192.168.1.16:~/FireBender/WebIOPi-0.7.1/frbndr/html/
+  inotifywait -r -e modify index.html -e modify config -e modify web_i2c.py
+
 done
--- web_i2c.py
+++ web_i2c.py
@@ -1,33 +1,53 @@
 import webiopi
-import os
-import sys
 from webiopi.devices.analog.pca9685 import PCA9685
-
-pca0 = PCA9685(0x40,500)
-
+val = 0
+val_prev = 0
+#pca0
+#pca0 = PCA9685(0x40,500)
+pca0 = webiopi.deviceInstance("pwm0")
 # setup function is automatically called at WebIOPi startup
 def setup():
-    return
+    pca0 = webiopi.deviceInstance("pwm0") # retrieve the device named "mcp" in the configuration
+    #return
 
 # loop function is repeatedly called by WebIOPi 
 def loop():
-    
-    max = pca0.pwmMaximum()      # returns PCA9685 maximum integer value
-    pca0.pwmWrite(0, max)        # set 100% on PCA9685 pwm channel 0 (integer value)
-    print max
-    print pca0
-    webiopi.sleep(3)
+    global val, val_prev
+    if (val != val_prev):
+    #max = pca0.pwmMaximum()      # returns PCA9685 maximum integer value
+        pca0.pwmWrite(0, val)        # set 100% on PCA9685 pwm channel 0 (integer value)
+        #webiopi.sleep(3)
+        val_prev = val
+    #print max
+    #print pca0
+    #webiopi.sleep(3)
 
-    pca0.pwmWrite(0, (max/2) )       # set 0% on PCA9685 pwm channel 0 (integer value)        
+    #pca0.pwmWrite(0, (max/2) )       # set 0% on PCA9685 pwm channel 0 (integer value)        
 
-    webiopi.sleep(3)
+    #webiopi.sleep(3)
 
-    pca0.pwmWrite(0, 0)        # set 0% on PCA9685 pwm channel 0 (integer value)        
-
-    webiopi.sleep(3)
-    # gives CPU some time before looping again
+    #pca0.pwmWrite(0, 0)        # set 0% on PCA9685 pwm channel 0 (integer value)        
+    #webiopi.sleep(3)
+    #gives CPU some time before looping again
     #webiopi.sleep(5)
+    #destroy function is called at WebIOPi shutdown
 
-# destroy function is called at WebIOPi shutdown
 def destroy():
+    pca0 = webiopi.deviceInstance("pwm0") # retrieve the device named "mcp" in the configuration
     return
+
+# a simple macro to return heater mode
+@webiopi.macro
+def start_py_Spark():
+    global val, val_prev
+    if (val == 0):
+        val = pca0.pwmMaximum()
+    else:
+        val = 0
+    print "Spark!" + str(val)
+    #pca0.pwmWrite(0, max)
+    #webiopi.sleep(_time_spark);
+    #webiopi.sleep(5)
+    #pca0.pwmWrite(0, 0)
+    return "Spark!"
+
